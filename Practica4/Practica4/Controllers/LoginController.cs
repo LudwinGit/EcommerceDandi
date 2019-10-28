94f0196b96c2ab20729deef95b571c9599d72ebc
huriel2g <hurieluv2g2d93@gmail.com>
2019-10-27 23:32:41 -0600

    Codificando Login y Perfil - 201403841

--- Practica4/Practica4/Controllers/LoginController.cs
+++ Practica4/Practica4/Controllers/LoginController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,18 +12,69 @@ namespace Practica4.Controllers
     public class LoginController : Controller
     {
         // GET: Login
-        public ActionResult Index()
+        static string server = "DESKTOP-39C8GSB";
+        // GET: Login
+        public ActionResult vLogin()
         {
+            if (Session["CUENTA"] != null)
+            {
+                return RedirectToAction("vPerfil", "Perfil");
+            }
+
             return View();
         }
 
-        public ActionResult Loguear(string v1, string v2)
+        public ActionResult Loguear(string user, string password)
         {
-            if(v1.Equals("30000003") && v2.Equals("CCCC0000"))
+            string consulta = "select usuario, nombres, apellidos, saldo, no_cuenta, password from usuario where no_cuenta =" + user + " and password = '" + password + "'";
+            DataTable dt = consultarBD(consulta);
+
+            if (dt.Rows.Count > 0)
             {
-                return View();
+                DataRow row = dt.Rows[0];
+                if (user.Equals(row["no_cuenta"].ToString()) && password.Equals(row["password"].ToString()))
+                {
+                    Session["ID"] = row["usuario"].ToString();
+                    Session["CUENTA"] = row["no_cuenta"].ToString();
+                    //Session["SALDO"] = row["saldo"].ToString();
+                    Session["NOMBRE"] = row["nombres"].ToString() + " " + row["apellidos"].ToString();
+                    Session["Error"] = null;
+                    return RedirectToAction("vPerfil", "Perfil");
+                }
             }
-            return null;
+            Session["Error"] = "Codigo de usuario o contraseña incorrecto..!!!";
+            return RedirectToAction("vLogin", "Login");
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            Session.Abandon();
+            return RedirectToAction("vLogin", "Login");
+        }
+        public static DataTable consultarBD(string Consulta)
+        {
+            string credenciales = "server=" + server + "; database=Practica3y4; integrated security = true";
+            SqlConnection conexion = new SqlConnection(credenciales);
+            SqlDataAdapter adaptador = new SqlDataAdapter();
+            DataTable ds = new DataTable();
+            try
+            {
+                conexion.Open();
+                SqlCommand sql = new SqlCommand();
+                sql.CommandText = Consulta;
+                sql.CommandType = CommandType.Text;
+                sql.Connection = conexion;
+
+                adaptador.SelectCommand = sql;
+                adaptador.Fill(ds);
+                conexion.Close();
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(Consulta);
+                return null;
+            }
         }
     }
 }
